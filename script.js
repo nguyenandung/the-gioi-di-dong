2db84d32075bacc6bb1c0834465361bb2e7b8b56
nguyenandung <txo432@gmail.com>
2022-02-17 13:56:07 +0700

    upload

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ const adressclose = document.querySelector('#adress-close');
 adressclose.addEventListener('click', function() {
     document.querySelector('.adress-form').style.display = 'none';
 })
+
+//slider  
+let index = 0;
+const rightbtn = document.querySelector('.fa-chevron-right');
+const leftbtn = document.querySelector('.fa-chevron-left');
+const imgNumber = document.querySelectorAll('.slider-content-left-top img');
+console.log(imgNumber);
+
+rightbtn.addEventListener("click", function() {
+    index = index+1;
+    if(index > imgNumber.length-1){
+        index=0;
+    }
+    document.querySelector('.slider-content-left-top').style.right = index*100 +"%";
+});
+leftbtn.addEventListener("click", function() {
+    index = index-1;
+    if(index <=0){
+        index = imgNumber.length-1;
+    }
+    document.querySelector('.slider-content-left-top').style.right = index*100+"%";
+});
+
